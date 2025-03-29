76b60676de399c9b8a10c7fd8c83e87182eb6fe6
Yeun <80025908+yeun210304@users.noreply.github.com>
2025-03-29 14:51:04 +0900

    first commit

---
+++ TradeBizCsv/src/main/java/com/TradeBizCsv/TradeBizCsvApplication.java
@@ -0,0 +1,13 @@
+package com.TradeBizCsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeBizCsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeBizCsvApplication.class, args);
+	}
+
+}
---
+++ TradeBizCsv/src/test/java/com/TradeBizCsv/TradeBizCsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TradeBizCsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeBizCsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
