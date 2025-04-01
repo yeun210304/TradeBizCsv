78f4a6f229aad40a30fc7ef9844bb6aa92b5f9c4
Yeun <80025908+yeun210304@users.noreply.github.com>
2025-04-01 16:15:28 +0900

    domian 재설정

--- src/main/java/com/TradeBizCsv/domain/TradeBizInf.java
+++ src/main/java/com/TradeBizCsv/domain/TradeBizInf.java
@@ -1,34 +1,38 @@
 package com.TradeBizCsv.domain;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.RequiredArgsConstructor;
 
+@Entity
 @Getter
 @Setter
-@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 public class TradeBizInf {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;            // id
 
-    @Column(name = "prmmiMnno")
+    @Column(name = "prmmiMnno", length = 20)
     private String prmmiMnno;   // 통신판매번호
 
-    @Column(name = "bzmnNm")
+    @Column(name = "bzmnNm", length = 100)
     private String bzmnNm;      // 상호
- 
-    @Column(name = "brno")
+
+    @Column(name = "brno", length = 10)
     private String brno;        // 사업자등록번호
 
-    @Column(name = "crno")
+    @Column(name = "crno", length = 13)
     private String crno;        // 법인등록번호
 
-    @Column(name = "admCd")
+    @Column(name = "admCd", length = 10)
     private String admCd;       // 행정구역코드
 
 }
