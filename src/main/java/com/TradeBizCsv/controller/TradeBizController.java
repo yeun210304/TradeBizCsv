ca580c8f1e9b31db9fb687d42a73ec7385b9eab9
Yeun <80025908+yeun210304@users.noreply.github.com>
2025-04-01 20:35:51 +0900

    api 동시 실행되게 변경

--- src/main/java/com/TradeBizCsv/controller/TradeBizController.java
+++ src/main/java/com/TradeBizCsv/controller/TradeBizController.java
@@ -2,7 +2,6 @@ package com.TradeBizCsv.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 import org.springframework.stereotype.Controller;
@@ -26,34 +25,40 @@ public class TradeBizController {
 
     @GetMapping("/")
     public String welcomPage() {
-        return "uploadform";
+        return "mainPage";
     }
 
-    @PostMapping("/load-csv")
+    @PostMapping("/saveTraceBizInf")
     public String loadCsvFile(@RequestParam("csvFile") MultipartFile file) {
 
-        List<TradeBizInf> tradeBizInfLst = new ArrayList<>();
-
         List<String[]> csv = tradeBizService.loadCsv(file);
+
+        List<CompletableFuture<TradeBizInf>> futures = new ArrayList<>();
         
         for (String[] row : csv) {
             String prmmiMnno = row[0];                                      // 통신판매번호
             String bzmnNm = row[2];                                         // 상호
             String brno = row[3].replaceAll("-", "");       // 사업자등록번호
+            String addr = row[10].split("\\^")[0]; 
 
+            // 법인등록번호
             CompletableFuture<String> crnoFuture = CompletableFuture.supplyAsync(() -> tradeBizService.getCrno(brno));
-            String crno = crnoFuture.join();                                // 법인등록번호
-            
-            String addr = row[10].split("\\^")[0];
+            // 행정구역코드
             CompletableFuture<String> admCdFuture = CompletableFuture.supplyAsync(() -> tradeBizService.getAdmCd(addr));
-            String admCd = admCdFuture.join();                              // 행정구역코드
 
-            tradeBizInfLst.add(new TradeBizInf(null, prmmiMnno, bzmnNm, brno, crno, admCd));
+            CompletableFuture<TradeBizInf> tradeBizFuture = crnoFuture.thenCombine(admCdFuture, 
+            (crno, admCd) -> new TradeBizInf(null, prmmiMnno, bzmnNm, brno, crno, admCd));
+
+            futures.add(tradeBizFuture);
         }
+
+        List<TradeBizInf> tradeBizInfLst = futures.stream()
+                .map(CompletableFuture::join)
+                .toList();
         
         tradeBizService.saveAllTradeBiz(tradeBizInfLst);
 
-        return "redirect:uploadform";
+        return "redirect:/";
     }
 
 }
