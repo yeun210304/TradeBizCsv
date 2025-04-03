31d4f43ceec15843b4c1470c3412b8ca4d6325d3
Yeun <80025908+yeun210304@users.noreply.github.com>
2025-04-03 17:20:28 +0900

    데이터조회 후 페이징처리

--- src/main/java/com/TradeBizCsv/controller/TradeBizController.java
+++ src/main/java/com/TradeBizCsv/controller/TradeBizController.java
@@ -1,16 +1,18 @@
 package com.TradeBizCsv.controller;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.TradeBizCsv.domain.TradeBizInf;
 import com.TradeBizCsv.service.TradeBizService;
@@ -31,7 +33,7 @@ public class TradeBizController {
     }
 
     @PostMapping("/saveTraceBizInf")
-    public String loadCsvFile(@RequestParam("csvFile") MultipartFile file, RedirectAttributes redirectAttributes) {
+    public String loadCsvFile(@RequestParam("csvFile") MultipartFile file, Model model, @PageableDefault(page = 0, size = 5)Pageable pageable) {
 
         List<String[]> csv = tradeBizService.loadCsv(file);
 
@@ -59,9 +61,22 @@ public class TradeBizController {
                 .toList();
         
         tradeBizService.saveAllTradeBiz(tradeBizInfLst);
+        model.addAttribute("resMsg", "총 " + tradeBizInfLst.size() + " 건이 저장되었습니다.");
 
-        redirectAttributes.addAttribute("resMsg", "총 " + tradeBizInfLst.size() + " 건이 저장되었습니다.");
-        return "redirect:/";
+        Page<TradeBizInf> allsavedInfs = tradeBizService.getAllsavedInfs(pageable);
+        model.addAttribute("page", allsavedInfs);
+        model.addAttribute("totalPages", allsavedInfs.getTotalPages());
+        
+        return "mainPage";
     }
+
+    @GetMapping("/pagedTradeBizInfs")
+    public String getPagedTradeBizInfs(Model model, @PageableDefault(page = 0, size = 5) Pageable pageable) {
+        Page<TradeBizInf> page = tradeBizService.getAllsavedInfs(pageable);
+        model.addAttribute("page", page);
+        model.addAttribute("totalPages", page.getTotalPages()); // 추가
+    return "mainPage";
+}
+
 
 }
